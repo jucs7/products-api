9d97de854a245d246a59355ee59826607d9f9112
jucs7 <jucs0709@gmail.com>
2025-05-17 16:26:19 -0500

    feat: created models

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,17 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
+
+Route::get('/', function () {
+    return 'API UP';
+});
+
+Route::apiResource('categories', CategoryController::class);
+Route::apiResource('products', ProductController::class);
