58fe4009526981495725ce0d1934298b93aa5810
jucs7 <jucs0709@gmail.com>
2025-05-20 15:39:30 -0500

    feat: applying layered arch with services and repositories

---
+++ app/Repositories/Eloquent/CategoryRepository.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Repositories\Eloquent;
+
+use App\Models\Category;
+use App\Repositories\CategoryRepositoryInterface;
+
+class CategoryRepository implements CategoryRepositoryInterface
+{
+    
+}
---
+++ app/Repositories/Eloquent/ProductRepository.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Repositories\Eloquent;
+
+use App\Models\Product;
+use App\Repositories\ProductRepositoryInterface;
+
+class ProductRepository implements ProductRepositoryInterface
+{
+    
+}
