7bf363cb4ffa8c7551b272e8fd818cd3f4eb7f2a
jucs7 <jucs0709@gmail.com>
2025-05-20 19:40:49 -0500

    feat: User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,9 +2,25 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Model
 {
-    //
+    use HasFactory, Notifiable, HasApiTokens;
+
+    protected $fillable = [
+        'name', 'email', 'password', 'role'
+    ];
+
+    protected $hidden = [
+        'password', 'remember_token'
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'role' => 'string',
+    ];
 }
